505956f67516296e12111dfeb87badb599439baf
Mafizulislambd <167568030+Mafizulislambd@users.noreply.github.com>
2024-09-14 20:29:05 +0600

    crud using popup and store procedure separate db con

---
+++ StoreprocedureCrudUsingSeparateConsting/Utility/ConnectionString.cs
@@ -0,0 +1,11 @@
+﻿namespace StoreprocedureCrudUsingSeparateConsting.Utility
+{
+    public class ConnectionString
+    {
+        private static string cName = "Data Source=Mafizul-Islam\\MSSQLSERVER01;Initial Catalog=StudentManagement;Integrated Security=True;Encrypt=False";
+        public static string CName
+        {
+            get => cName;
+        }
+    }
+}
