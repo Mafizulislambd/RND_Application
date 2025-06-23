7dfe4f2e7912f48bb396e337ba3d80288812c1ee
Mafizulislambd <167568030+Mafizulislambd@users.noreply.github.com>
2025-06-23 14:03:04 +0600

    Modified

--- HomeRentTracker/Models/RenterEntity/MemberInfo.cs
+++ HomeRentTracker/Models/RenterEntity/MemberInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using HomeRentTracker.Models.Enumation;
+using System.ComponentModel.DataAnnotations;
 
 namespace HomeRentTracker.Models.RenterEntity
 {
@@ -6,14 +7,24 @@ namespace HomeRentTracker.Models.RenterEntity
     {
         [Key]
         public int MemberId { get; set; }
+        [Display(Name = "Name")]
         public string MemberName { get; set; } = string.Empty;
-        public int MemberAge { get; set; }
+        [Display(Name = "Age")]
+        public int? MemberAge { get; set; }
+        [Display(Name = "Phone")]
         public string MemberPhone { get; set; } = string.Empty;
+        [Display(Name = "Email")]
         public string? MemberEmail { get; set; }
+        [Display(Name = "National ID")]
         public string MemberNID { get; set; } = string.Empty;
+        [Display(Name = "Address")]
         public string? MemberAddress { get; set; }
-        public string MemberProfession { get; set; } = string.Empty;
+        [Display(Name = "Profession")]
+        public Profession MemberProfession { get; set; }
+        //    public string MemberProfession { get; set; } = string.Empty;
+        [Display(Name = "Guardian ID")]
         public string GuardianID { get; set; } = string.Empty;
+        [Display(Name = "Guardian NID")]
         public string? GuardialNID { get; set; }
         public string? RelationWithGuardian { get; set; }
     }
